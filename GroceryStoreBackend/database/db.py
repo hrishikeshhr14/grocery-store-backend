345277f57026cc1560543feeb8e73e1cb5487eac
HRISHIKESH D <111124015+hrishikeshhr14@users.noreply.github.com>
2025-06-02 17:59:11 +0530

    Update db.py

--- GroceryStoreBackend/database/db.py
+++ GroceryStoreBackend/database/db.py
@@ -4,8 +4,7 @@ import psycopg2
 
 load_dotenv()
 
-DB_URL = os.getenv("DB_URL")
-
+DB_URL = os.getenv("DATABASE_URL")  # Use DATABASE_URL for compatibility with Render
 
 def get_connection():
     return psycopg2.connect(DB_URL)
